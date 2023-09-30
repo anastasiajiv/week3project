ee7892e9ebfc8c8eeee5c874a5f2e177abcbb4ec
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-09-29 23:34:57 -0400

    edit to make call output the same as figure 2

--- Calling_API.java
+++ Calling_API.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 class Calling_Api {
     public static void main(String[] args) {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.spoonacular.com/recipes/findByNutrients?minSugar=20&minSodium=50&number=1"))
+                .uri(URI.create("https://api.spoonacular.com/recipes/715424/information?apiKey=bc2478d0611242ecac099d1e39b017fb"))
                 .header("x-api-host", "https://api.spoonacular.com")
-                .header("x-api-key", " f2a5ff240c244409a72b0c15164189d5")
+                .header("x-api-key", " bc2478d0611242ecac099d1e39b017fb")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = null;
